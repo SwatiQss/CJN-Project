a5d01a527ca471f736170d1349df3e92833d7681
Swati Tiwari <swati.tiwari@qsstechnosoft.com>
2025-01-20 14:44:49 +0530

    2

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+import './page.css'
+//Blank merge page
+const HomePage = () => {
+  return (
+    <div className='merge-page'>
+     <p className='heading'>Master Name Index</p>
+     <h3 className='sub-heading'>Master Name Record Merge</h3>
+    </div>
+  );
+};
+export default HomePage;
